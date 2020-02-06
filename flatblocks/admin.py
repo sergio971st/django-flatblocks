cb034de5d1a810826ecf1a524e5a66c9a5638252
Sergey <s.stepennov@whyfly.ru>
2020-02-06 19:28:44 +0300

    custom admin_register method added

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from flatblocks.models import FlatBlock
 
 
-class FlatBlockAdmin(admin.ModelAdmin):
-    ordering = ['slug', ]
-    list_display = ('slug', 'header', 'subdomain', 'content', 'all_subdomains')
-    list_filter = ('all_subdomains',)
-    search_fields = ('slug', 'header', 'content', 'subdomain')
+def admin_register_flatblocks(filter_list=None):
+    class FlatBlockAdmin(admin.ModelAdmin):
+        ordering = ['slug', ]
+        list_display = ('slug', 'header', 'subdomain', 'content', 'all_subdomains')
+        if filter_list:
+            list_filter = filter_list
+        search_fields = ('slug', 'header', 'content', 'subdomain')
 
-admin.site.register(FlatBlock, FlatBlockAdmin)
+    admin.site.register(FlatBlock, FlatBlockAdmin)
