3473e9f4adc89e3c44a1ff95d6c2dbd9af7602a2
Sergey <s.stepennov@whyfly.ru>
2020-02-12 15:23:24 +0300

    improvements

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
+from django.conf import settings
 from flatblocks.models import FlatBlock
 
 
-def admin_register_flatblocks(filter_list=None):
-    class FlatBlockAdmin(admin.ModelAdmin):
-        ordering = ['slug', ]
-        list_display = ('slug', 'header', 'subdomain', 'content', 'all_subdomains')
-        if filter_list:
-            list_filter = filter_list
-        search_fields = ('slug', 'header', 'content', 'subdomain')
+class FlatBlockAdmin(admin.ModelAdmin):
+    ordering = ['slug', ]
+    list_display = ('slug', 'header', 'subdomain', 'content', 'all_subdomains')
+    list_filter = ('all_subdomains',)
+    search_fields = ('slug', 'header', 'content', 'subdomain')
 
+
+if not settings.CANCAEL_ADMIN_AUTO_REGISTRATION:
     admin.site.register(FlatBlock, FlatBlockAdmin)
